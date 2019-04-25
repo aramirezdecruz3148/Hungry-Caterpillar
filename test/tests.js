8ab450648284bf2aa06d6a7d83fdff0349b69b64
aramirezdecruz3148 <leigh3148@gmail.com>
2019-04-25 13:26:12 -0700

    I accidentally copied the wrong tests.js content, so I updated it

--- test/tests.js
+++ test/tests.js
@@ -1,23 +1,13 @@
-import getHeadsOrTails from '../src/get-heads-or-tails.js';
 const test = QUnit.test;
 
-test('gets heads for less than .5', (assert) => {
+test('', (assert) => {
     // arrange
-    const number = .49;
-    const expected = 'heads';
+  
 
     // act
-    const result = getHeadsOrTails(number);
+
 
     // assert
     assert.equal(result, expected);
 });
 
-test('returns tails for equal or greater than .5', (assert) => {
-    const number = .5;
-    const expected = 'tails';
-
-    const result = getHeadsOrTails(number);
-
-    assert.equal(result, expected);
-});
