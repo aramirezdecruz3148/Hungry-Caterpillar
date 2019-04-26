64edaf1f8736419fd9002e121d4f5818b5d5fe00
aramirezdecruz3148 <leigh3148@gmail.com>
2019-04-26 08:16:25 -0700

    I forgot to put my listening event into the for loop, I have moved it and now my buttons are wired and able to console.log

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ const colorButtons = document.querySelectorAll('.color');
 //initializing stuff and creating my loop for my buttons
 for(let i = 0; i < colorButtons.length; i++) {
     const colorButton = colorButtons[i];
-}
 
-//wiring up listening event
-colorButton.addEventListener('click', () => {
-    console.log(colorButton.value);
-});
+    //wiring up listening event
+    colorButton.addEventListener('click', () => {
+        console.log(colorButton.value);
+    });
+}
