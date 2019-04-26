49fcb035cdd99696c73447c4ec74ade19b023888
aramirezdecruz3148 <leigh3148@gmail.com>
2019-04-26 11:34:13 -0700

    I added to my original function test to include passing the span element an additional class for manipulation later

--- src/vomit-unicorn.js
+++ src/vomit-unicorn.js
@@ -1,7 +1,8 @@
-function vomitUnicorn(Mockunicorn, colorName) {
+function vomitUnicorn(Mockunicorn, colorName, puke) {
     const span = document.createElement('span');
     span.classList.add('part');
     span.classList.add(colorName);
+    span.classList.add(puke);
     Mockunicorn.appendChild(span);
 }
 
--- test/tests.js
+++ test/tests.js
@@ -1,15 +1,18 @@
 import vomitUnicorn from '../src/vomit-unicorn.js';
 const test = QUnit.test;
 
-test('test to cause vomit balls to be created from my buttons', (assert) => {
+test('test to cause vomit balls to be created && to add additional class to move puke', (assert) => {
     // arrange
     const colorName = 'red';
     const mockUnicorn = document.createElement('div');
-    const expected = '<span class="part red"></span>';
+    const puke = 'puke';
+    const expected = '<span class="part red puke"></span>';
     // act
-    vomitUnicorn(mockUnicorn, colorName); 
+    vomitUnicorn(mockUnicorn, colorName, puke); 
     // assert
     const parentInnerHTML = mockUnicorn.innerHTML;
     assert.equal(parentInnerHTML, expected);
 });
+
+
 
