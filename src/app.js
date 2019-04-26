c9dbef7861d158ea6551324b1493b46f14d41ce9
aramirezdecruz3148 <leigh3148@gmail.com>
2019-04-26 16:33:20 -0700

    I added my added my new button and my span elements to my cast list, wrote a for loop to go through my spans, and wired up my new button

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import vomitUnicorn from './vomit-unicorn.js';
 //selecting elements I want to use, (posting the cast list)
 const colorButtons = document.querySelectorAll('.color');
 const unicorn = document.getElementById('unicorn');
+const pukes = document.getElementsByClassName('.part');
+const cleanerButton = document.getElementById('puke-cleaner');
 
 //initializing stuff and creating my loop for my buttons
 for(let i = 0; i < colorButtons.length; i++) {
@@ -10,5 +12,15 @@ for(let i = 0; i < colorButtons.length; i++) {
     //wiring up listening event
     colorButton.addEventListener('click', () => {
         vomitUnicorn(unicorn, colorButton.value);
+    });
+}
+
+//creating a for loop to loop through my span elements
+for(let i = 0; i < pukes.length; i++) {
+    const puke = pukes.length[i];
+
+    //wiring up listening event
+    cleanerButton.addEventListener('click', () => {
+
     });
 }
